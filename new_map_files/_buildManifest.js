b89b959a3e9618090354aa206599a4c3aae752d0
Pierre Schutz <pierreschutz@rocketmail.com>
2024-02-01 21:10:07 -0800

    macbook_2019_2024

---
+++ new_map_files/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static\u002Fchunks\u002Fpages\u002Findex.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/new_map":["static\u002Fchunks\u002Fpages\u002Fnew_map.js"],sortedPages:["\u002F","\u002F_app","\u002F_error","\u002Fnew_map"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
